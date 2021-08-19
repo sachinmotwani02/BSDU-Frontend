5ce7ee859db0bea11f66d785c5fe31f77e98f76a
Sachin Motwani <sachinmotwani60@gmail.com>
2021-08-19 15:35:54 +0530

    login screen added

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { useState } from 'react'
 import { EditText } from 'react-edit-text';
 import SideNav from './components/SideNav';
 import EditScreen from './screens/EditScreen';
+import LoginScreen from './screens/LoginScreen';
 import School from './screens/School';
 
 function App() {
 
-  const [name, setName] = useState('School of Automobile');
-  const [desc, setDesc] = useState('Lorem ipsum oder isdum.');
-
   return (
     <div>
       {/* <EditScreen /> */}
-      <School />
+      {/* <School /> */}
+      <LoginScreen />
     </div>
   );
 }
